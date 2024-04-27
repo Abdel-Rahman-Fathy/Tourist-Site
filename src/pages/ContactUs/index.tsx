ae05191712972fa02f66fd07b733da3552a84ab8
Abdel-Rahman Fathy <105522928+Abdel-Rahman-Fathy@users.noreply.github.com>
2024-04-27 15:01:52 +0300

    add contact section

--- src/pages/ContactUs/index.tsx
+++ src/pages/ContactUs/index.tsx
@@ -1,13 +1,31 @@
 import { useTranslation } from "react-i18next";
 import FixedSection from "../../Components/FixedSection";
-
+import { Box, Stack, Typography } from "@mui/material";
+import contactUs2 from "../../assets/ContactUs2.jpg";
 function ContactUsPage() {
   const [t] = useTranslation();
 
   return (
-    <>
+    <Stack>
       <FixedSection title={t("contactUs")} />
-    </>
+      <Box
+        style={{
+          backgroundImage: `url(${contactUs2})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center center",
+          backgroundRepeat: "no-repeat",
+          padding: "100px 0px",
+        }}
+        sx={{ textAlign: "center", py: "50px" }}
+      >
+        <Typography variant="h6" sx={{ fontSize: "45px", fontWeight: 600 }}>
+          CONTACT US
+        </Typography>
+        <Typography variant="body1" sx={{ fontSize: "25px" }}>
+          You will be answered
+        </Typography>
+      </Box>
+    </Stack>
   );
 }
 
--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -6,18 +6,19 @@ import Footer from "../../Components/Footer";
 import { Route, Routes } from "react-router-dom";
 import AboutUs from "../AboutUs";
 import ContactUsPage from "../ContactUs";
+import FixedIcon from "../main/components/FixedIcon";
 
 function Layout() {
   return (
     <div>
       <Navbar />
-      {/* <NavbarContainers /> */}
       <div className="main-view">
         <Routes>
           <Route path="" element={<MainPages />} />
           <Route path="about" element={<AboutUs />} />
           <Route path="contact" element={<ContactUsPage />} />
         </Routes>
+        <FixedIcon />
       </div>
       <Footer />
     </div>
