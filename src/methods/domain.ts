1bbc0aa2c3e2215ae7d209ce6eaa3d0c1713e53f
Abdel-Rahman Fathy <105522928+Abdel-Rahman-Fathy@users.noreply.github.com>
2024-06-27 15:59:54 +0300

    change sub domain

--- src/methods/domain.ts
+++ src/methods/domain.ts
@@ -1,2 +1,3 @@
 export const domain = (path?: string) =>
-  `https://react.hanstarcarrental.com${path ? "/" + path : ""}`;
+  // `https://react.hanstarcarrental.com ${path ? "/" + path : ""}`;
+  `https://egyptos-travel.com ${path ? "/" + path : ""}`;
